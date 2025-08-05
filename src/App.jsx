6a6b8e2badbbeaf64f8d957a606d8eba3fdbb29e
Shruti gupta <guptashruti7788@gmail.com>
2025-08-05 12:01:19 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import { useState } from 'react'
-import Navbar from './Components/Navbar'
-import Home from './Components/home'
-import FinanceBanner from './Components/FinanceBanner'
-import DeliveredProjects from './Components/DeliveredProjects'
-import TestimonialsSection from './Components/TestimonialsSection'
-import ServiceSection from './Components/ServiceSection'
-import FAQSection from './Components/FAQSection'
-import Footer from './Components/Footer'
-
+import { useState } from "react";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/home";
+import FinanceBanner from "./Components/FinanceBanner";
+import DeliveredProjects from "./Components/DeliveredProjects";
+import TestimonialsSection from "./Components/TestimonialsSection";
+import ServiceSection from "./Components/ServiceSection";
+import FAQSection from "./Components/FAQSection";
+import Footer from "./Components/Footer";
 
 function App() {
- 
-
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <FinanceBanner/>
-  <DeliveredProjects/>
-  <TestimonialsSection/>
-  <ServiceSection/>
-  <FAQSection/>
-  <Footer/>
-  
+      <Navbar />
+      <Home />
+      <FinanceBanner />
+      <DeliveredProjects />
+      <TestimonialsSection />
+      <ServiceSection />
+      <FAQSection />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
